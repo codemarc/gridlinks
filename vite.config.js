c71772c6df60ebecc150d48a02edafbd2bf6acb8
marc j. greenberg <marc@buildinglink.com>
2024-06-17 06:25:30 -0400

    chunking based on node module name

--- vite.config.js
+++ vite.config.js
@@ -23,5 +23,16 @@ export default defineConfig({
   plugins: [react(), crx({ manifest })],
   server: {
     port: 3000
-  }
+  },
+  build: {
+   rollupOptions: {
+       output:{
+           manualChunks(id) {
+               if (id.includes('node_modules')) {
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString()
+               }
+           }
+       }
+      }
+   }
 })
