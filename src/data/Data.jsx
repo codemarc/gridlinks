e853c7c3d813448b493e9d0330f2dc1614990466
marc j. greenberg <marc@buildinglink.com>
2024-06-20 13:21:30 -0400

    design data

---
+++ src/data/Data.jsx
@@ -0,0 +1,21 @@
+import p1 from "./p1.json"
+import p2 from "./p2.json"
+import p3 from "./p3.json"
+import p4 from "./p4.json"
+import p5 from "./p5.json"
+import p6 from "./p6.json"
+import p7 from "./p7.json"
+import p8 from "./p8.json"
+
+const pdata = {
+   p1: p1,
+   p2: p2,
+   p3: p3,
+   p4: p4,
+   p5: p5,
+   p6: p6,
+   p7: p7,
+   p8: p8
+}
+
+export default pdata
