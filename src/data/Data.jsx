215ab464bff2a6cbecae9ae89be03e190b3d06e2
marc j. greenberg <marc@buildinglink.com>
2024-08-21 17:36:42 -0400

    a little data cleanup

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -1,48 +1,34 @@
 // import blank from "./blank.yaml"
 // import p0 from "./p0.yaml"
-import p1 from "./comms.yaml"
-import p2 from "./devtools.yaml"
-import p3 from "./genai.yaml"
-import p4 from "./devops.yaml"
-import p5 from "./p5.yaml"
-import p6 from "./amazon.yaml"
-import p7 from "./apache.yaml"
-import p8 from "./shopify.yaml"
 
-
+import amzn from "./amzn.yaml"
 import apache from "./apache.yaml"
 import banking from "./banking.yaml"
 import cloud from "./cloud.yaml"
 import comms from "./comms.yaml"
+import devops from "./devops.yaml"
+import devtools from "./devtools.yaml"
 import entertain from "./entertain.yaml"
 import fintech from "./fintech.yaml"
 import genai from "./genai.yaml"
 import google from "./google.yaml"
 import house from "./house.yaml"
+import msft from "./msft.yaml"
 import services from "./services.yaml"
 import shopify from "./shopify.yaml"
 
 
-const sdata = {
-   p1: p1,
-   p2: p2,
-   p3: p3,
-   p4: p4,
-   p5: p5,
-   p6: p6,
-   p7: p7,
-   p8: p8,
-}
+const sdata = {}
 
 export default function pdata(ls) {
    if (!ls.gs) {
       // default - builtin
-      sdata.p1 = p1
-      sdata.p2 = p2
+      sdata.p1 = comms
+      sdata.p2 = devtools
       sdata.p3 = genai
-      sdata.p4 = p4
-      sdata.p5 = p5
-      sdata.p6 = p6
+      sdata.p4 = devops
+      sdata.p5 = msft
+      sdata.p6 = amzn
       sdata.p7 = google
       sdata.p8 = apache
    } else {
